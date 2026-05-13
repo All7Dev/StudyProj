2d9cd26ddffe5f8f735abc7470b0c3dcfd464959
All7Dev <all7m7dell@gmail.com>
2026-05-13 08:48:33 +0300

    feat: добавлн дамп БД

--- src/main/java/com/example/SmartHouse/config/SecurityConfig.java
+++ src/main/java/com/example/SmartHouse/config/SecurityConfig.java
@@ -53,8 +53,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**", "/webjars/**", "/upload.html").permitAll()
-    .anyRequest().authenticated()
+                .requestMatchers("/api/auth/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**", "/webjars/**", "/upload.html").permitAll()
             )
                 .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
